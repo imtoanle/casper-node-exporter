0cec64b8e7e000ee87b85c9b645f127b6f6aa4d3
Toan Le <ktoanlba@gmail.com>
2021-08-14 20:34:33 +0700

    update env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+const SERVICE_NAME = process.env.SERVICE_NAME || "casper-validator-metrics";
+const OTEL_ENDPOINT = process.env.OTEL_ENDPOINT || "http://127.0.0.1:55681/v1/trace";
+
 module.exports = {
   apps: [{
-    name: "casper-validator-metrics",
+    name: SERVICE_NAME,
     script: "./server_init.js",
     watch: true,
     env: {
       OTEL_METRICS_EXPORTER: "none",
-      OTEL_EXPORTER_OTLP_SPAN_ENDPOINT: "http://10.0.100.104:55681/v1/trace",
-      LS_SERVICE_NAME: "casper-validator-metrics"
+      OTEL_EXPORTER_OTLP_SPAN_ENDPOINT: OTEL_ENDPOINT,
+      LS_SERVICE_NAME: SERVICE_NAME
     }
   }]
 }
